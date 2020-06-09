d85e8dc015c6c130557ef07c8fd0b7ba68154d04
Ian Mubangizi <io@ianmubangizi.com>
2020-06-09 22:39:32 +0200

    ui changes: on the footer, header, login, dashboard

--- src/View/templates/includes/header.php
+++ src/View/templates/includes/header.php
@@ -1,16 +1,23 @@
+<?php require_once "helpers/functions.php";
+$current = get_current_page();
+$static = $current->static_files;
+?>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title></title>
+    <title><?php echo $current->title ?></title>
     <!-- CSS only -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
-          integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <link rel="stylesheet" href="/static/css/bootstrap.min.css">
+    <?php foreach ($static['css'] as $ref): ?>
+    <link rel="stylesheet" href="/static/css/<?php echo $ref ?>">
+    <?php endforeach; ?>
 </head>
 <body>
-
-<header>
-    <?php include "partials/navbar.php"; ?>
-</header>
+<?php if (!is_current('index')): ?>
+    <header>
+        <?php include "partials/navbar.php"; ?>
+    </header>
+<?php endif; ?>
