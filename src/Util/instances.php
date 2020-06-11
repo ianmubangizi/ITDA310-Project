e7a63504c53209e152df07c590c0bd35c82aac2c
Ian Mubangizi <io@ianmubangizi.com>
2020-06-11 15:03:37 +0200

    x .env

--- src/Util/instances.php
+++ src/Util/instances.php
@@ -7,6 +7,3 @@ use Hospital\View\Core\Route;
 $route = Route::instance();
 $index = Index::instance();
 $dashboard = Dashboard::instance();
-
-$env = Dotenv\Dotenv::createImmutable($_SERVER['WEB_DOCUMENT_ROOT']);
-$env->load();
